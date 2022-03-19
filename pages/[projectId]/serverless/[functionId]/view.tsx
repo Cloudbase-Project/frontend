a17990d78e40cb6ed73d9930f1f1830533f30e7d
Navaneeth V <vnavaneeth15@gmail.com>
2022-03-20 00:05:40 +0530

    add view function page

--- pages/[projectId]/serverless/[functionId]/view.tsx
+++ pages/[projectId]/serverless/[functionId]/view.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Editor from '@monaco-editor/react';
+import Link from 'next/link';
 
 export default function ViewFunction() {
 	return (
@@ -8,7 +9,7 @@ export default function ViewFunction() {
 				<h1 className='text-6xl text-center font-normal leading-normal mt-0 mb-2 text-blueGray-800'>
 					My Project
 				</h1>
-				<div className='flex justify-between mx-64'>
+				<div className='flex justify-between my-7 mx-64'>
 					<h1 className='  text-4xl font-normal leading-normal mt-0 mb-2 text-blueGray-800'>
 						Hello world Function
 					</h1>
@@ -19,12 +20,85 @@ export default function ViewFunction() {
 					</button>
 				</div>
 
+				<div className='mx-64'>
+					<div>
+						<div className='w-full flex justify-between  '>
+							<div className='font-bold text-2xl my-6'>
+								Configuration
+							</div>
+							<Link href={'/asd/serverless/functionid/logs'}>
+								<div className='underline font-bold text-lg text-brand-green py-8 cursor-pointer'>
+									View Logs
+								</div>
+							</Link>
+						</div>
+						<div className='p-4 ml-4 flex justify-between px-14 pr-8 mt-2 text-lg border rounded-md hover:bg-slate-50 hover:shadow-sm '>
+							<div>Runtime :</div>{' '}
+							<select name='threshold'>
+								<option value='javascript'>Javascript</option>
+								<option value='go'>Golang</option>
+								<option value='python'>Python</option>
+							</select>
+						</div>
+						<div className='p-4 ml-4 flex justify-between px-14 mt-2 text-lg border rounded-md hover:bg-slate-50 hover:shadow-sm '>
+							<div>Deployed Number of Instances :</div>{' '}
+							<div>4</div>
+						</div>
+						<div className='p-4 ml-4 flex justify-between px-14 pr-8 mt-2 text-lg border rounded-md hover:bg-slate-50 hover:shadow-sm '>
+							<div>Maximum Number of Instances :</div>{' '}
+							<input
+								className='w-9 '
+								type={'number'}
+								defaultValue={10}
+								max={10}
+								min={1}
+							/>
+						</div>
+						<div className='p-4 ml-4 flex justify-between px-14 pr-8 mt-2 text-lg border rounded-md hover:bg-slate-50 hover:shadow-sm '>
+							<div>Minimum Number of Instances :</div>{' '}
+							<input
+								className='w-9 '
+								type={'number'}
+								defaultValue={2}
+								max={10}
+								min={0}
+							/>
+						</div>
+						<div className='p-4 ml-4 flex justify-between px-14 pr-8 mt-2 text-lg border rounded-md hover:bg-slate-50 hover:shadow-sm '>
+							<div>Scaling :</div>{' '}
+							<select name='threshold'>
+								<option value='CPU'>CPU</option>
+								<option value='Memory'>Memory</option>
+								<option value='Requests per Second'>
+									Requests per Second
+								</option>
+							</select>
+						</div>
+						<div className='p-4 ml-4 flex justify-between px-14 pr-4 mt-2 text-lg border rounded-md hover:bg-slate-50 hover:shadow-sm '>
+							<div>Threshold for Scaling :</div>{' '}
+							<div>
+								<input
+									className='w-10'
+									type={'number'}
+									defaultValue={25}
+									max={100}
+									min={25}
+								/>{' '}
+								%
+							</div>
+						</div>
+					</div>
+
+					<div className='w-full font-bold text-2xl mt-10 '>
+						Add Your Code Here
+					</div>
+				</div>
 				<div
-					className='flex justify-center mt-10
+					className='flex justify-center mt-5 mx-64
                 '>
 					<Editor
 						height='500px'
-						width='75%'
+						width='100%'
 						defaultLanguage='javascript'
 						defaultValue={`import express from "express"
 
@@ -37,7 +111,7 @@ app.get("/", (_, res) => {
 						options={{
 							fontSize: 20,
 							padding: {
-								top: 16,
+								top: 25,
 								bottom: 5,
 							},
 						}}
